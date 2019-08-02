9b5d2bb33e56600a92782a5e16d8c4235b0f8818
Thomas Johnson <tjohnson1106@gmail.com>
2019-08-02 16:41:43 -0400

    setup apollo provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Micro Dev</h1>
     </div>
   );
 }
-
-export default App;
