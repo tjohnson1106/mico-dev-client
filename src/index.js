cba26e1201a1cb5d1225758c7e3147232cea60f4
Thomas Johnson <tjohnson1106@gmail.com>
2019-08-05 17:18:39 -0400

    created Menu links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
 import ApolloProvider from "./ApolloProvider";
 import * as serviceWorker from "./serviceWorker";
 
---
+++ src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <h1>Register Pages</h1>
+    </div>
+  );
+}
+
+export default Register;
